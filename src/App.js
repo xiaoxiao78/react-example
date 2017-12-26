5d19fcdd23d3829ab017ad46511b6e106580664f
huangxiaoxiao <ljyxiaoxiao@gmail.com>
2017-12-26 17:21:45 +0800

    css modules   配置

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import Author from '_/components/author'
 import Category from '_/components/category'
 import Book from '_/components/book'
 
-import '_/style/index.sass'
+import style from '_/style/index.sass'
+import _header from '_/style/header.sass'
+import _menu from '_/style/menu.sass'
 
 function App() {
   return  (
     <BrowserRouter>
       <div>
-        <div>
-          <Link to="/author">作者</Link>
-          <Link to="/category">分类</Link>
-          <Link to="/book">书籍</Link>
+        <div className={_header.container}>
+        <Link to="/author">作者</Link>
+        <Link to="/category">分类</Link>
+        <Link to="/book">书籍</Link>
         </div>
         <div>
           <Route path="/author" component={Author} />
           <Route path="/category" component={Category} />
-          <Route path="/author" component={Author} />
+          <Route path="/book" component={Book} />
         </div>
       </div>
     </BrowserRouter>
